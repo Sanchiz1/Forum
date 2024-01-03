a74540583fb66b141c9025a8affdc5b71680323f
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-01-03 17:51:56 +0200

    Restructure

---
+++ tests/Application.UnitTests/Users/CreateUserCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Application.Common.Models;
+using Application.UseCases.Users.Commands;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Users
+{
+    public class CreateUserCommandTests
+    {
+        private static readonly CreateUserCommand Command = new CreateUserCommand()
+        {
+            Username = "testUsername",
+            Email = "testemail@gmail.com",
+            Bio = "testBio",
+            Password = "testPassword"
+        };
+
+        private readonly CreateUserCommandHandler _handler;
+        private readonly IUserRepository _userRepositoryMock;
+
+        public CreateUserCommandTests()
+        {
+            _userRepositoryMock = Substitute.For<IUserRepository>();
+
+            _handler = new CreateUserCommandHandler(_userRepositoryMock);
+        }
+
+        [Fact]
+        public async Task Handle_Create_Account_Success()
+        {
+            var result = await _handler.Handle(Command, default);
+
+            var res = result.IfFail("");
+            Assert.Equal("Account has been created successfully", res);
+        }
+    }
+}
