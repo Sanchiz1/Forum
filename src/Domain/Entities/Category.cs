0545bbd5e3bf96fdcd64bff373e33139b44605d4
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-01-08 21:14:52 +0200

    Added entity rules

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Common;
+using Domain.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,18 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Category
+    public class Category : BaseEntity
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public Category() { }
+
+        public bool CanEdit(string Account_Role)
+        {
+            return Account_Role == Roles.Admin;
+        }
+        public bool CanDelete(string Account_Role)
+        {
+            return Account_Role == Roles.Admin;
+        }
     }
 }
--- src/Domain/Entities/Post.cs
+++ src/Domain/Entities/Post.cs
@@ -1,16 +1,27 @@
-﻿using Domain.Constants;
+﻿using Domain.Common;
+using Domain.Constants;
 using System;
 
 namespace Domain.Entities
 {
-    public class Post
+    public class Post : BaseEntity
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime Date_Created { get; set; }
         public DateTime? Date_Edited { get; set; }
         public int User_Id { get; set; }
         public Post() { }
+
+        public bool CanEdit(int Account_Id)
+        {
+            return this.User_Id == Account_Id;
+        }
+        public bool CanDelete(int Account_Id, string Account_Role)
+        {
+            return this.User_Id == Account_Id
+                || Account_Role == Roles.Moderator
+                || Account_Role == Roles.Admin;
+        }
     }
 }
